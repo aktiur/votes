5d99bb378b690844bfca48b0a1debb2c75fdb4b8
Arthur Cheysson <arthur@cheysson.fr>
2015-12-11 09:06:24 +0100

    Rend le parser fonctionnel pour récupérer les intitulés + nombre de votes

--- votes_spider.py
+++ votes_spider.py
@@ -5,7 +5,7 @@ import re
 
 __author__ = 'Arthur Cheysson <arthur.cheysson@opusline.fr>'
 
-RE_VOTE = r'([0-9]+)(*?)'
+RE_VOTE = r'([0-9]+)(\*?)'
 
 class VotesSpider(scrapy.Spider):
     name = "vote"
@@ -18,14 +18,22 @@ class VotesSpider(scrapy.Spider):
 
     def parse(self, response):
         for ligne in response.css('table.scrutins tbody tr'):
-            denom = ligne.css('.denom::text')
-            desc = ligne.css('.desc::text')
+            denom = ligne.css('.denom::text').extract_first()
+            desc = ligne.css('.desc::text').extract_first()
 
-            score_pour = int(ligne.css('.pour::text'))
-            score_contre = int(ligne.css('.contre::text'))
-            score_abs = int(ligne.css('.abs::text'))
+            score_pour = int(ligne.css('.pour::text').extract_first())
+            score_contre = int(ligne.css('.contre::text').extract_first())
+            score_abs = int(ligne.css('.abs::text').extract_first())
 
             match = self.re_vote.match(denom)
             identifiant_vote = match.group(1)
             solennel = match.group(2) != ''
 
+            yield {
+                'identifiant': identifiant_vote,
+                'solennel': solennel,
+                'desc': desc,
+                'pour': score_pour,
+                'contre': score_contre,
+                'abstention': score_abs
+            }
